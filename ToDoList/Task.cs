f16ad4d814c7c4059a9e1034d3dedd7e0aab06b2
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2023-11-09 09:38:53 +0100

    Add project files.

---
+++ ToDoList/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class Task
+    {
+        public string Title { get; set; }
+        public string DueDate { get; set; }
+        public string Project { get; set; }
+        public bool Done { get; set; }
+    }
+}
